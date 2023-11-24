bae881745b1135328481a8efeacc646f658fa0cb
shishanchen <shisc@nancal.com>
2023-11-24 17:51:29 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Date: 2022-03-17 19:35:27
+ * @LastEditTime: 2022-03-17 19:36:24
+ * @FilePath: \js\imageHandler\tailwind.config.js
+ */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
